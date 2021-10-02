a38bc4be81cfcdc43b3b1b69f807c36156f0284b
Liew Jun Tung <juntung@netvirta.com>
2021-10-02 18:20:45 +0800

    feat: :sparkles: done with processItem

--- contracts/coffeebase/SupplyChain.sol
+++ contracts/coffeebase/SupplyChain.sol
@@ -182,15 +182,17 @@ contract SupplyChain is FarmerRole {
   }
 
   // Define a function 'processtItem' that allows a farmer to mark an item 'Processed'
-  function processItem(uint _upc) public 
+  function processItem(uint _upc)  public 
   // Call modifier to check if upc has passed previous supply chain stage
-  
+  harvested(_upc)
   // Call modifier to verify caller of this function
-  
+   onlyFarmer
   {
     // Update the appropriate fields
+    items[_upc].itemState = State.Processed;
     
     // Emit the appropriate event
+    emit Processed(upc);
     
   }
 
