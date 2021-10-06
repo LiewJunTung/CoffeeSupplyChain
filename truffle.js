5ec4bdd19d66aa7d17e7f5e8d589a9e9f8ab92b6
JT <liewjuntung@gmail.com>
2021-10-06 22:39:27 +0800

    build: update truffle.js for deployment

--- truffle.js
+++ truffle.js
@@ -1,3 +1,8 @@
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".mnemonic").toString().trim();
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const infuraKey = fs.readFileSync(".infuraKey").toString().trim();
+
 module.exports = {
   networks: {
     development: {
@@ -9,6 +14,14 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: 5777
+    },
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`)
+      },
+      network_id: 4,
+      gas: 4500000,
+      gasPrice: 10000000000
     }
   },
   compilers: {
